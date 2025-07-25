26c606e9ef32ef63f052995c5c772d964a35dc89
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-07-25 08:42:25 -0600

    main

--- src/laboratorio_programacion2_samuel_said/main.java
+++ src/laboratorio_programacion2_samuel_said/main.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package laboratorio_programacion2_samuel_said;
-
+import javax.swing.*;
+import java.awt.*;
+      
 /**
  *
  * @author unwir
  */
-public class main {
+public class main extends JFrame{
     
+    public main(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        JLabel a = new JLabel();
+        a.setText("HOLA MUNDO");
+        
+        add(a);
+    }
+    
+    public static void main(String[] args){
+        main b = new main();
+        b.setVisible(true);
+    } 
 }
