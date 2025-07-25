d0020f41586334d745525c0bdb5fb5a24c1f54a8
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-07-25 09:48:46 -0600

    arregloBotones

---
+++ src/laboratorio_programacion2_samuel_said/botonsito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio_programacion2_samuel_said;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ *
+ * @author unwir
+ */
+public class botonsito extends JButton{
+    public botonsito(String text){
+        super(text);
+    }
+}
---
+++ src/laboratorio_programacion2_samuel_said/tablaBotones.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio_programacion2_samuel_said;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author unwir
+ */
+public class tablaBotones extends JFrame{
+    botonsito[] aseintosVisuales = new botonsito[30];
+  
+    public tablaBotones(){
+        setSize(800, 800);
+        setLayout(new GridLayout(6, 5));
+        setLocationRelativeTo(null);
+        
+        for (int i = 0; i < 30; i++) {
+            botonsito b = new botonsito("Asiento" + i);
+            aseintosVisuales[i] = b;
+            b.setBackground(Color.green);
+            int indice = i;
+            b.addActionListener(e -> {
+            
+            
+            
+            });
+            
+            
+            add(b);
+        }
+        
+    }
+    
+    public static void main (String[] args){
+        tablaBotones tb = new tablaBotones();
+        tb.setVisible(true);
+     
+    
+    }
+    
+}
